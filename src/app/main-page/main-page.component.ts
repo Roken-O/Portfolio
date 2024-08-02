5086329af82d18866c9e5125e7256639b8fd4c00
Roken-O <roken.othman91@gmail.com>
2024-08-02 09:41:29 +0200

    added header, footer, skills

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { ReferencesComponent } from "./references/references.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [AboutMeComponent, SkillsComponent, ProjectsComponent, ContactComponent, ProfileComponent, ReferencesComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
