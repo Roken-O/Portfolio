8cd0e4ed221efc68ddb79d322d61f20d8367837d
Roken-O <roken.othman91@gmail.com>
2024-08-18 21:59:18 +0200

    modified Responsive

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'roken.othman91@gmail.com';  
+            $recipient = 'mail@roken-othman.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
